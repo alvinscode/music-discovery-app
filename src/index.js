1521fe262222ab047cfb71b223947bfe04512895
Alvin Li <aflip24@gmail.com>
2023-07-02 06:32:54 -0500

    add dependencies

--- src/index.js
+++ src/index.js
@@ -9,3 +9,14 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
+
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./server/db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001; 
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
