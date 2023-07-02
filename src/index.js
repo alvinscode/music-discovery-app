f0f1b3da6df604ba38d08d870eea35bef517387f
Alvin Li <aflip24@gmail.com>
2023-07-02 07:10:54 -0500

    fix json

--- src/index.js
+++ src/index.js
@@ -9,15 +9,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
-
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('../server/db.json');
-
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running');
-});
