5857e910936f6f214c56794a577825e1d7920a83
Alvin Li <aflip24@gmail.com>
2023-07-02 06:44:54 -0500

    json server

--- src/index.js
+++ src/index.js
@@ -10,13 +10,14 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('./server/db.json');
+
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; 
 
 server.use(middlewares);
 server.use(router);
-
-server.listen(port);
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
